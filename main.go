1c685ac9ae76fc9201ade4855eb8420432d0270e
Ben Berry <dragonben7@gmail.com>
2024-08-13 23:17:33 -0500

    basic server running 404

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	fmt.Println("Hello world")
+	serverMux := http.NewServeMux()
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: serverMux,
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
